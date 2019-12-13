27c0dc7a93c665a547ed0ce2510045a29335ebc5
bolivara <bolivara@mit.edu>
2019-12-13 18:22:17 -0500

    parser and w1s1 patht

--- paths.py
+++ paths.py
@@ -2,49 +2,52 @@ def push(obj, l, depth):
     while depth:
         l = l[-1]
         depth -= 1
-
     l.append(obj)
 
 def path_parser(path_str):
     groups = []
     depth = 0
-    # l = []
-    s1 = ""
-    s2  = ""
+    s1,s2 = "",""
     s2_ = False
-    a = False #add
-    s1_ = True
-    for char in path_str:
-        if char == '(':
-            push([], groups, depth)
-            depth += 1
-            s2_=False
-            s1 = ""
-            s2 = ""
-            s1_ = False
-        elif char == ')':
-            depth -= 1
-            # l.append((s1,s2))
-            s2_=False
-            s1_ = False
+    done,start = False,False
+    nums = "-1234567890."
+    for char in range(len(path_str)-1):
+        if path_str[char] == '(' and path_str[char+1] in nums:
+            start = True
+        elif path_str[char] == ')' and path_str[char-1] in nums:
+            pass
         else:
-            if char != ",":
-                if s2_:
-                    s2+=char
-                else:
-                    s1+=char
-            else:
-                s2_ = True
-            if s2_ and len(s2)>17 :
-                push((s1,s2), groups, depth)
-    # return groups
+            if start:
+                if len(s2)<17:
+                    done = False
+                    if s2_:
+                        s2+=path_str[char]
+                    else:
+                        if path_str[char]==" ":
+                            s2_ = True
+                        else:
+                            s1+=path_str[char]
+                elif len(s2)==17:
+                    done  = True
+                    s2_ = False
+                if done:
+                    push((s1,s2), groups, depth)
+                    s2 = ""
+                    s1 = ""
+                    start = False
+            if path_str[char] == '(':
+                push([], groups, depth)
+                depth += 1
+            elif path_str[char] == ')':
+                depth -= 1
+
     return groups
 
-import path_data
+import world1_path
 
-path_str = path_data.path1
-path_str = path_str.replace(" ", ",")
-print(path_parser(path_str))
+path_str = world1_path.path_1
 
-
-# print(path_parser('(a(b(c)(d)e)(f)g)'))
+g = (path_parser(path_str))
+for j in g[0]:
+    print(j[0])
+    print(j[1])
